375164418ca278e778311272ddcb2cd1802ce5ba
sameerwatts1992 <sameer@arintra.com>
2023-09-25 18:20:57 +0530

    Parsing request bodies

--- server.js
+++ server.js
@@ -11,14 +11,22 @@ const method = req.method
   if(url === '/') {
     res.write('<html lang="en">');
     res.write('<head><title>Enter message</title></head>');
-    res.write('<body><form action="/message" method="POST"><input type="text"/><button type="submit">Send</button></form></body>');
+    res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
     res.write('</html>');
     return res.end();
   }
-  if(url === '/message' && method=== 'POST' ) {
-    // redirect to /
+  if(url === '/message' && method === 'POST' ) {
+    const body = [];
+    req.on('data', (chunk) => {
+      body.push(chunk);
+    });
 
-    fs.writeFileSync('message.txt', 'hello');
+    req.on('end', () => {
+      const parseBody = Buffer.concat(body).toString();
+      const message = parseBody.split("=")[1];
+      fs.writeFileSync('message.txt', message);
+    })
+
     res.statusCode = 302;
     res.setHeader('Location', '/');
     return res.end();
