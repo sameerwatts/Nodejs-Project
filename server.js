c3aae1b2a26d6a0a1ff164ac6bd24b584b2b0187
sameerwatts1992 <sameer@arintra.com>
2023-09-25 17:03:48 +0530

    Sending responses

--- server.js
+++ server.js
@@ -6,6 +6,12 @@ const http = require('http');
 const server = http.createServer((req, res) => {
   console.log(req.url, req.method, req.headers);
   // process.exit();
+  res.setHeader('Content-Type', 'text/html');
+  res.write('<html lang="en">');
+  res.write('<head><title>First page</title></head>');
+  res.write('<body><h1>Hello from nodejs server</h1></body>');
+  res.write('</html>');
+  res.end();
 })
 
 server.listen(3002);
