f817fe4cff56b9e6a574608a8cef1a6ccffaa7d3
sameerwatts1992 <sameer@arintra.com>
2023-09-25 20:40:39 +0530

    Using the node module system

--- server.js
+++ server.js
@@ -1,43 +1,7 @@
 const http = require('http');
-const fs = require('fs');
+const routes = require('./routes');
 
-// request listener
-
-
-const server = http.createServer((req, res) => {
-  // process.exit();
-const url = req.url;
-const method = req.method
-  if(url === '/') {
-    res.write('<html lang="en">');
-    res.write('<head><title>Enter message</title></head>');
-    res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
-    res.write('</html>');
-    return res.end();
-  }
-  if(url === '/message' && method === 'POST' ) {
-    const body = [];
-    req.on('data', (chunk) => {
-      body.push(chunk);
-    });
-
-    return req.on('end', () => {
-      const parseBody = Buffer.concat(body).toString();
-      const message = parseBody.split("=")[1];
-      fs.writeFile('message.txt', message, (err) => {
-        res.statusCode = 302;
-        res.setHeader('Location', '/');
-        return res.end();
-      });
-    })
-
-  }
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<html lang="en">');
-    res.write('<head><title>First page</title></head>');
-    res.write('<body><h1>Hello from nodejs server</h1></body>');
-    res.write('</html>');
-    res.end();
-})
+console.log(routes.someText)
+const server = http.createServer(routes.handler);
 
 server.listen(3002);
