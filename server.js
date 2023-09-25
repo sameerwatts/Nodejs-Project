d05ea1d7b934601a0027014cb4a669c2792277c2
sameerwatts1992 <sameer@arintra.com>
2023-09-25 17:32:45 +0530

    Routing requests

--- server.js
+++ server.js
@@ -4,14 +4,21 @@ const http = require('http');
 
 
 const server = http.createServer((req, res) => {
-  console.log(req.url, req.method, req.headers);
   // process.exit();
-  res.setHeader('Content-Type', 'text/html');
-  res.write('<html lang="en">');
-  res.write('<head><title>First page</title></head>');
-  res.write('<body><h1>Hello from nodejs server</h1></body>');
-  res.write('</html>');
-  res.end();
+const url = req.url;
+  if(url === '/') {
+    res.write('<html lang="en">');
+    res.write('<head><title>Enter message</title></head>');
+    res.write('<body><form action="/message" method="POST"><input type="text"/><button type="submit">Send</button></form></body>');
+    res.write('</html>');
+    return res.end();
+  }
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html lang="en">');
+    res.write('<head><title>First page</title></head>');
+    res.write('<body><h1>Hello from nodejs server</h1></body>');
+    res.write('</html>');
+    res.end();
 })
 
 server.listen(3002);
