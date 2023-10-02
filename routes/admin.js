72f496cf632b1a180f20f7aa2c2550d35b7d0670
sameerwatts1992 <sameer@arintra.com>
2023-10-02 19:36:07 +0530

    Use a helper function for navigation

--- routes/admin.js
+++ routes/admin.js
@@ -2,13 +2,15 @@ const path = require('path');
 
 const express = require('express');
 
+const rootDir = require('../utils/path');
+
 const app = express();
 
 const router = express.Router();
 
 // /admin-add-product => GET
 router.get("/add-product",(req, res, next) => {
-  res.sendFile(path.join(__dirname, "../", "views", "add-product.html"));
+  res.sendFile(path.join(rootDir, "views", "add-product.html"));
 })
 
 // /admin-add-product => POST
