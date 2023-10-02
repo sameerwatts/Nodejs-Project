57bc3be2ad7aa9d24964e0eb4c46afb1d6aa3fc8
sameerwatts1992 <sameer@arintra.com>
2023-10-02 20:56:30 +0530

    serve files statically

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const rootDir = require('./utils/path');
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, "public")))
 
 app.use("/admin", adminRoute);
 app.use(shopRoute);
